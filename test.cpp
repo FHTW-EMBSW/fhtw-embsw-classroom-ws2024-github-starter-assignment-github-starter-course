e1de2f5b9f2bf150198477774a9cdb8ed891e6d8
HeisenBerger1 <el22b012@technikum-wien.at>
2024-09-10 10:32:26 +0200

    removed commands

--- test.cpp
+++ test.cpp
@@ -1,12 +1,11 @@
 
 // C++ program to display "Hello World"
  
-// Header file for input output functions
+
 #include <iostream>
 using namespace std;
  
-// Main() function: where the execution of
-// program begins
+
 int main()
 {
     // Prints hello world
